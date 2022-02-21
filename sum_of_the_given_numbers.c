efd526526f46cfc03e3e2071ebe12f8324fe0a69
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-02-22 01:20:14 +0530

    #include<stdio.h> int main() {     int n,x,y;     scanf("%d",&n);
        while(n>0)
        {
            scanf("%d %d",&x,&y);
            
            printf("%d
    ",(x+y));
            n--;
        }
        return 0;
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,y;
+    scanf("%d",&n);
+    
+    while(n>0)
+    {
+        scanf("%d %d",&x,&y);
+        
+        printf("%d
+",(x+y));
+        n--;
+    }
+    return 0;
+}
