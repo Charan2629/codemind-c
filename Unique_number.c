565b2d1c1c02707811ba8cec63c90efa3d842b4e
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-04-26 21:44:01 +0530

    #include<stdio.h> int main() {     int n,x[10]={0},i,found=0,r;     scanf("%d",&n); while(n>0) {     r=n%10;     x[r]++;     n=n/10; } for(i=0;i<9;i++) {     if(x[i]>1)     {         found=1;         break;     } } if(found==0) {     printf("Unique Number"); } else {     printf("Not Unique Number"); } }

--- Unique_number.c
+++ Unique_number.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int n,x[10]={0},r,i,found=0;
+    int n,x[10]={0},i,found=0,r;
     scanf("%d",&n);
-    
-    while(n>0)
+while(n>0)
+{
+    r=n%10;
+    x[r]++;
+    n=n/10;
+}
+for(i=0;i<9;i++)
+{
+    if(x[i]>1)
     {
-        r=n%10;
-        x[r]++;
-        n=n/10;
+        found=1;
+        break;
     }
-    for(i=0;i<9;i++)
-    {
-        if(x[i]>1)
-          {
-              found=1;
-              break;
-          }
-    }
-    if(found==0)
-       printf("Unique Number");
-    else
-       printf("Not Unique Number");
-   return 0;
+}
+if(found==0)
+{
+    printf("Unique Number");
+}
+else
+{
+    printf("Not Unique Number");
+}
 }
