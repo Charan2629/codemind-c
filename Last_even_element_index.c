84213bc3d752413df1ba106d155444bfc2f09482
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-05-17 23:43:59 +0530

    #include<stdio.h> int main() {     int a[50],n,i,j,temp;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=n-1;i>=0;i--)     {         if(a[i]%2==0)         {             temp=i;             break;         }     }     printf("%d",temp); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],n,i,j,temp;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>=0;i--)
+    {
+        if(a[i]%2==0)
+        {
+            temp=i;
+            break;
+        }
+    }
+    printf("%d",temp);
+}
