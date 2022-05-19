64558cbe36fba998f98087a1b65e0852e05a1d90
Charan2629 <97590397+Charan2629@users.noreply.github.com>
2022-05-19 11:15:49 +0530

    #include<stdio.h> int main() {     int n,a[50],sum=0,avg,c=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum=sum+a[i];     }     avg=sum/n;     for(i=0;i<n;i++)     {         if(a[i]<=avg)         {             c++;         }     }     printf("%d",c);
    }

---
+++ elements_smaller_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[50],sum=0,avg,c=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    avg=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=avg)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
